f4b323c37f06f919344b36429a1e5184c705a76f
danysmall <dany.small.work@gmail.com>
2022-12-01 22:51:13 +0300

    Modified preferences from old API to the new one

--- main.js
+++ main.js
@@ -14,52 +14,51 @@
  * TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  *
  */
-define(function (require, exports, module) {
-	"use strict";
 
-	//# Import libs
-	var AppInit					= app.getModule("utils/AppInit");
-    var CommandManager      	= app.getModule("command/CommandManager");
-    var MenuManager        		= app.getModule("menu/MenuManager");
-    var DefaultMenus            = app.getModule("menu/DefaultMenus");
-    var ContextMenuManager      = app.getModule("menu/ContextMenuManager");
-    var ExtensionUtils          = app.getModule("utils/ExtensionUtils");
-    var Repository              = app.getModule("core/Repository");
+const init = () => {
+    //# Import libs
+    var AppInit = app.getModule("utils/AppInit");
+    var CommandManager = app.getModule("command/CommandManager");
+    var MenuManager = app.getModule("menu/MenuManager");
+    var DefaultMenus = app.getModule("menu/DefaultMenus");
+    var ContextMenuManager = app.getModule("menu/ContextMenuManager");
+    var ExtensionUtils = app.getModule("utils/ExtensionUtils");
+    var Repository = app.getModule("core/Repository");
 
     //Imported files
-    var TeamworkCommandHandler 	= require("./commandHandler/CommandHandler");
-    var LockingAttributesElement= require("./locking/LockingAttributesElement");
-    var Trigger                 = require("./trigger/Trigger");
-    var TeamworkView            = require("./teamworkView/TeamworkView");
+    var TeamworkCommandHandler = require("./commandHandler/CommandHandler");
+    var LockingAttributesElement = require("./locking/LockingAttributesElement");
+    var Trigger = require("./trigger/Trigger");
+    var TeamworkView = require("./teamworkView/TeamworkView");
 
     //Overwrites
-    Repository.doOperation  = Trigger.doOperation;
+    Repository.doOperation = Trigger.doOperation;
 
     //# Define Commands
-    var CMD_TEAMWORK         	= 'teamwork';
-    var CMD_TEAMWORK_COMMIT  	= 'teamwork.commit';
-    var CMD_TEAMWORK_OPEN       = 'teamwork.open';
-    var CMD_TEAMWORK_UPDATE  	= 'teamwork.update';
-    var CMD_TEAMWORK_CREATE  	= 'teamwork.create';
-    var CMD_TEAMWORK_LOCK  	    = 'teamwork.lock';
-    var CMD_UPDATE_LOCKS  	    = 'teamwork.lock.update';
-    var CMD_TEAMWORK_CONFIGURE  = 'teamwork.configure';
+    var CMD_TEAMWORK = 'teamwork';
+    var CMD_TEAMWORK_COMMIT = 'teamwork.commit';
+    var CMD_TEAMWORK_OPEN = 'teamwork.open';
+    var CMD_TEAMWORK_UPDATE = 'teamwork.update';
+    var CMD_TEAMWORK_CREATE = 'teamwork.create';
+    var CMD_TEAMWORK_LOCK = 'teamwork.lock';
+    var CMD_UPDATE_LOCKS = 'teamwork.lock.update';
+    var CMD_TEAMWORK_CONFIGURE = 'teamwork.configure';
 
-    var CMD_LOCK_ELEMENT        = 'teamwork.element.lock';
-    var CMD_UNLOCK_ELEMENT      = 'teamwork.element.unlock';
+    var CMD_LOCK_ELEMENT = 'teamwork.element.lock';
+    var CMD_UNLOCK_ELEMENT = 'teamwork.element.unlock';
 
     //# register commands
-    CommandManager.register("Teamwork",             		CMD_TEAMWORK,           CommandManager.doNothing);
-    CommandManager.register("Open Teamwork-Project...",     CMD_TEAMWORK_OPEN,      TeamworkCommandHandler.openProject);
-    CommandManager.register("Create Teamwork-Project", 	    CMD_TEAMWORK_CREATE,  	TeamworkCommandHandler.createProject);
-    CommandManager.register("Lock Teamwork-Project", 	    CMD_TEAMWORK_LOCK,  	TeamworkCommandHandler.lockWholeProject);
-    CommandManager.register("Update Lock-Info", 	        CMD_UPDATE_LOCKS,  	    TeamworkCommandHandler.updateProjectLockInfo);
-    CommandManager.register("Commit changes",  			    CMD_TEAMWORK_COMMIT,   	TeamworkCommandHandler.commitChanges);
-    CommandManager.register("Update Project",     		    CMD_TEAMWORK_UPDATE, 	TeamworkCommandHandler.updateProject);
-    CommandManager.register("Configure Teamwork-Server",    CMD_TEAMWORK_CONFIGURE, TeamworkCommandHandler.handleConfigure);
+    CommandManager.register("Teamwork", CMD_TEAMWORK, CommandManager.doNothing);
+    CommandManager.register("Open Teamwork-Project...", CMD_TEAMWORK_OPEN, TeamworkCommandHandler.openProject);
+    CommandManager.register("Create Teamwork-Project", CMD_TEAMWORK_CREATE, TeamworkCommandHandler.createProject);
+    CommandManager.register("Lock Teamwork-Project", CMD_TEAMWORK_LOCK, TeamworkCommandHandler.lockWholeProject);
+    CommandManager.register("Update Lock-Info", CMD_UPDATE_LOCKS, TeamworkCommandHandler.updateProjectLockInfo);
+    CommandManager.register("Commit changes", CMD_TEAMWORK_COMMIT, TeamworkCommandHandler.commitChanges);
+    CommandManager.register("Update Project", CMD_TEAMWORK_UPDATE, TeamworkCommandHandler.updateProject);
+    CommandManager.register("Configure Teamwork-Server", CMD_TEAMWORK_CONFIGURE, TeamworkCommandHandler.handleConfigure);
 
-    CommandManager.register("Lock Element",                 CMD_LOCK_ELEMENT,       TeamworkCommandHandler.lockElement);
-    CommandManager.register("Unlock Element",               CMD_UNLOCK_ELEMENT,     TeamworkCommandHandler.unlockElement);
+    CommandManager.register("Lock Element", CMD_LOCK_ELEMENT, TeamworkCommandHandler.lockElement);
+    CommandManager.register("Unlock Element", CMD_UNLOCK_ELEMENT, TeamworkCommandHandler.unlockElement);
 
     var menu;
     menu = MenuManager.addMenu(CMD_TEAMWORK);
@@ -83,7 +82,7 @@ define(function (require, exports, module) {
     contextMenuExplorer.addMenuItem(CMD_LOCK_ELEMENT);
     contextMenuExplorer.addMenuItem(CMD_UNLOCK_ELEMENT);
 
-    AppInit.appReady(function() {
+    AppInit.appReady(function () {
         TeamworkView.init();
     });
 
@@ -99,4 +98,4 @@ define(function (require, exports, module) {
         Trigger.setupTriggerCreateProject();
         //Trigger.setupTriggerOnDiagramChanges();
     });
-});
+}
